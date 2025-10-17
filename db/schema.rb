23240cdbd96db5d0f481b671dabca6cef432fd95
Floriane Diem <florianediem@gmail.com>
2025-10-16 20:22:48 -0400

    message

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 20_251_015_002_341) do
-  create_table 'restaurants', force: :cascade do |t|
-    t.string 'name'
-    t.string 'address'
-    t.string 'phone_number'
-    t.string 'category'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+ActiveRecord::Schema[7.1].define(version: 2025_10_15_002341) do
+  create_table "restaurants", force: :cascade do |t|
+    t.string "name"
+    t.string "address"
+    t.string "phone_number"
+    t.string "category"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
+
 end
